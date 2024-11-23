dae90abec58dc4d5f5bc91734c05992514448eb9
DeveloperAbhishekRajpoot <dev.abhishek.rajpoot@gmail.com>
2024-11-23 19:51:19 +0530

    Created the global templates and static folders to keep header and footer details seprate

--- codesphere/settings.py
+++ codesphere/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'codesphere.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'codesphere.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  # Your database name (default is "postgres")
-        'USER': 'postgres',  # Your PostgreSQL username (default is "postgres")
-        'PASSWORD': 'abhi@C9J',  # Your PostgreSQL password
-        'HOST': 'localhost',  # Default host
-        'PORT': '5432',  # Default PostgreSQL port
+        'NAME': 'postgres',  
+        'USER': 'postgres',  
+        'PASSWORD': 'abhi@C9J',  
+        'HOST': 'localhost',  
+        'PORT': '5432',     
     }
 }
 
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
