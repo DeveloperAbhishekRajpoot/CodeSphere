f6ef6f46c209785f98b5cdb52b7fbec205e8a483
DeveloperAbhishekRajpoot <dev.abhishek.rajpoot@gmail.com>
2024-11-29 20:06:19 +0530

    added some css to header

--- landingPage/views.py
+++ landingPage/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def explore(request):
-    return HttpResponse("lets begin with our explore page")
+    return render(request , 'landing_page/explore.html')
